50c820b65b082bafac802f3e10c5836fcaf865bd
gurinder613 <52985254+gurinder613@users.noreply.github.com>
2019-09-13 10:47:18 +1200

    admin panel modal

--- api/routes/users.js
+++ api/routes/users.js
@@ -81,7 +81,7 @@ if (user.length < 1) { // return res.status(401).json({ // message: "Auth failed
   }
   if (result) {
   const token = jwt.sign({
-  email: user[0].email,
+        email: user[0].email,
   userId: user[0]._id
   },
   process.env.JWT_KEY, {
